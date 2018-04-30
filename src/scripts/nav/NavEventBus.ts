e3cdd92649895785b6c3623daa7de0cd8b33a66c
Lief Swanson <liefs@sfu.ca>
2018-04-29 18:07:34 -0700

    remove old todo that was inaccurate

--- src/scripts/nav/NavEventBus.ts
+++ src/scripts/nav/NavEventBus.ts
@@ -1,8 +1,5 @@
 import Vue from "vue";
-// FIXME
-// this file is in src/ rather than in its proper place
-// due to a bug in vetur path finding
-// should go in src/components/nav/NavEventBus.ts
+
 const NavEventBus = new Vue();
 
 export default NavEventBus;
