3cafcfadbcc7a8afbff590056ed45393655acb63
Lief Swanson <liefs@sfu.ca>
2018-05-19 14:50:17 -0700

    fix about page overscroll, and selection

--- dev/src/components/nav/NavBar.vue
+++ dev/src/components/nav/NavBar.vue
@@ -92,8 +92,8 @@ $blinder-opacity: 0.3;
     position: fixed;
     top: 0;
     left: 0;
-    height: 100%;
-    width: 100%;
+    height: 100vh;
+    width: 100vw;
     @include on-laptop-or-up {
         display: none;
     }
