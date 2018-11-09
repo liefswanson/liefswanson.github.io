b9e06d97272bbfe5a4fd470a523f5df5e5e29f58
Lief Swanson <liefs@sfu.ca>
2018-11-09 02:00:11 -0800

    improve project spacing, rewrite some projects

--- dev/src/components/projects/RecursivePlug.vue
+++ dev/src/components/projects/RecursivePlug.vue
@@ -1,110 +1,39 @@
 <template>
 <article class='p-article-grid'>
-    <h1 class='p-wide p-title'>
-        The Portfolio You are Currently Viewing
-    </h1>
+        <h1 class='p-wide p-title'>
+            Designing and Implementing this portfolio
+        </h1>
 
-    <p class='p-wide p-highlight'>
-        Check it out on
-        <v-link to='https://github.com/liefswanson/liefswanson.github.io'>My GitHub<i class='p-icon fab fa-github'/></v-link>
-    </p>
+        <p class='p-text p-wide'>
+            By this point, you are probably reasonably acquainted with me and my portfolio!
+            If not, I recommend exploring some more after perusing my recursive self-advertising.
+            I am at least mildly interesting, I swear.
+        </p>
 
-    <p class='p-wide p-text'>
-        This portfolio is itself a portfolio piece.
-        I spent a lot of time researching many different things to make this website:
-    </p>
+        <h2 id='what' class='p-wide p-sub-title'>
+            What my role was
+        </h2>
 
-    <ul class='p-wide p-list'>
-        <li>Vue</li>
-        <li>TypeScript</li>
-        <li>Webpack</li>
-        <li>SCSS</li>
-        <li>CSS flex</li>
-        <li>CSS grid</li>
-        <li>Lot of other things...</li>
-    </ul>
+        <p class='p-text p-wide'>
+            Everything.
+            My goal was to create a thorough portfolio that speaks to my wide range of interests and abilities, without making me look full of myself.
+            The best way I could see to do that was to display my skills, instead of talk about them.
+        </p>
 
-    <p class='p-thin p-note'>
-        This wasn't my first time using most of these, but it was the first time I got the chance to really delve into them.
-        This was my 3rd time writing a responsive website.
-    </p>
+        <h2 id='why' class='p-wide p-sub-title'>
+            Why this project is important to me
+        </h2>
 
-    <p class='p-wide p-text'>
-        This website does have some pretty cool features which I want to point out just in case you didn't notice.
-    </p>
+        <p class='p-text p-wide'>
+            I don’t like to talk about myself that much.
+            I prefer to talk about the things I am interested in.
+            However, a portfolio is like a product page… my time, perspective, and knowledge are the products being sold, and as such those are what I need to talk about.
+            It was a fulfilling challenge to express who I am through the interactions, functionality, visuals, and content of this portfolio.
+        </p>
 
-    <ul class='p-wide p-list'>
-        <li>
-            The primary colour changes dynamically
-            <ul class='p-list'>
-                <li>text highlighting</li>
-                <li>color of my name</li>
-                <li>primary colour inherited by text</li>
-                <li>
-                    the <b>favicon</b>
-                    <span class='p-note'>as long as your browser's image cache plays nice</span>
-                </li>
-            </ul>
-        </li>
-        <li>
-            The header and nav bar can hide
-            <span class='p-note'>freeing up screen real estate while reading</span>
-        </li>
-        <li>You can use touch gestures,
-            via <v-link to='https://www.npmjs.com/package/vue2-touch-events'>vue2-touch-events</v-link>,
-            to open and close the nav bar</li>
-        <li>The entire website is a single page app</li>
-        <li>
-            I wrote my own
-            <ul class='p-list'>
-                <li>variable height collapsible divs</li>
-                <li>
-                    custom
-                    <v-link to='https://masonry.desandro.com/'>Masonry</v-link> style layout
-                </li>
-                <li>image carousel</li>
-                <li>media queries <span class='p-note'>in both SCSS and TypeScript</span></li>
-                <li>Sticky ribbon and automatically hiding header</li>
-            </ul>
-        </li>
-        <li>The site makes use of inline SVGs and font-awesome</li>
-        <li>I have custom fonts loaded from the Google Font Library</li>
-        <li>
-            The PDF of my resume displays in page via
-            <v-link to='https://github.com/mozilla/pdf.js'>PDF.js</v-link>
-        </li>
-    </ul>
-
-
-    <v-carousel class='p-mid-incremental p-image'
-                relative='/static/recursive-plug/'
-                :init="[
-                    'thumb.svg',
-                    'orange.svg',
-                    'blue.svg',
-                    'pink.svg',
-                    'green.svg',
-                    'red.svg'
-                ]"/>
-
-    <p class='p-thin-grow p-note p-push-right'>
-        It isn't perfect, but it is a pretty nice image carousel for my first attempt right?
-        <br/>
-        <br/>
-        Also the logos aren't bad!
-        Maybe a bit hyperbolic, but they still look pretty good I think... and they are definitely recognizable.
-    </p>
-
-    <p class='p-wide p-text'>
-        I did make a few concessions with my design decisions.
-        The entire website is static, not because I don't know how to use a website backend, but because I didn't want to pay for hosting.
-        GitHub will provide free static hosting and you can even use a custom domain name.
-        I also ended up doing some strange things in the source to accommodate the design having a dynamic primary colour.
-    </p>
-
-    <p class='p-thin p-note'>
-        Such as dynamically updating a style tag and link tag in the website header.
-    </p>
+        <h2 id='how' class='p-wide p-sub-title'>
+            How I tackled it
+        </h2>
 
 </article>
 </template>
--- dev/src/components/projects/TeachersAssistant.vue
+++ dev/src/components/projects/TeachersAssistant.vue
@@ -1,73 +1,74 @@
 <template>
     <article class='p-article-grid'>
         <h1 class='p-wide p-title'>
-            1 TA / 90 Students
+            Teaching Like a Pro
         </h1>
-        <p class='p-text p-wide'>
-            I was the TA for a second year class in Object Oriented Programming at SFU.
-            Actually, I was the only TA, and there were 90 students.
-        </p>
 
         <p class='p-text p-wide'>
-            This presented an uncommon challenge.
-            It is pretty difficult to engage with 90 students and mark them effectively, thoroughly, and consistently.
-            To mark them effectively, you need to be fast.
-            To mark them thoroughly, will obviously slow you down.
-            To mark them consistently, you must maintain the same standard and enthusiasm with every student.
+            I was an undergrad teaching assistant at Simon Fraser University.
+            I worked with 90 students, helping them learn Object Oriented Programming in Java.
+            As I was the only TA, and thus only marker, marking consistently and fairly became a herculean task.
+            To achieve this, I automated a huge portion of my job, so I could spend time on the good parts, such as giving feedback on code style and thoroughly responding to students’ questions.
+            Without automation, even just downloading, unzipping, compiling, and running every student’s code would have taken more than 3 hours total per assignment.
         </p>
+
+        <p class='p-thin-grow p-note'>
+            Being an undergrad teaching assistant at Simon Fraser University is not very common.
+            The teaching assistants' union created a rule that <strong>every</strong> graduate student be considered for a position before an undergrad is allowed to apply.
+        </p>
+
+        <h2 id='what' class='p-wide p-sub-title'>
+            What my role was
+        </h2>
 
         <p class='p-text p-wide'>
-            I came about solving this problem by automating everything I could.
-            For instance, there was a Battleship-style game where students would randomize the positions of ships.
-            To mark this consistently, I requested an addition to the assignment: the option of cheating at the game.
-            I had the enemy positions displayed and output into a python script which parsed the board.
-            Then I used a reverse implementation of the assignment I wrote to play the exact same game for every student.
-            In other words, I undid the randomization of enemy positions, to play the same game for each student.
-            I even played the game automatically by having xdotool do the input for me, to avoid making mistakes.
-        </p>
-
-        <p class='p-text p-wide'>
-            To automate marking effectively for each assignment, there was a lot of work involved.
-            I used quite a few tools to my advantage:
-        </p>
-
-        <ul class='p-list p-wide'>
-            <li>
-                <v-link to='https://fishshell.com/'>Fish</v-link>
-                & Bash
-            </li>
-            <li>Python <span class='p-note'>especially the core I/O, regex, and JSON libraries</span></li>
-            <li>GNU coreutils</li>
-            <li>grep</li>
-            <li><v-link to='https://github.com/jordansissel/xdotool'>xdotool</v-link></li>
-            <li><v-link to='https://github.com/yahoo/blink-diff'>Yahoo's blink-diff</v-link></li>
-            <li>git diff</li>
-            <li><v-link to='https://www.kde.org/applications/graphics/spectacle/'>KDE spectacle</v-link></li>
-            <li>and more!</li>
-        </ul>
-
-        <p class='p-note p-thin'>
-            That might seem like a pretty complicated mess, but I assure you, it made for a pretty reasonable testing harness and marking tool.
-        </p>
-
-        <p class='p-text p-wide'>
-            I also learned a whole lot from reading students' code.
-            It is a really unusual experience to really read and understand the code of 90 people who were all trying to do the exact same thing.
+            Marking student's projects
+            <ul class='p-list'>
+                <li>Commenting on code style</li>
+                <li>Providing refactoring suggestions &amp; examples</li>
+                <li>Giving detailed marking feedback</li>
+                <li>Building my own test suite &amp; harness to mark with</li>
+                <li>Reverse engineering project specifications as part of verifying outputs</li>
+                <li>Debugging broken code submissions <span class='p-note'>so part marks could be given where appropriate</span></li>
+                <li>Catching cheaters</li>
+            </ul>
+            <br>
+            Helping Students online and in person
+            <ul class='p-list'>
+                <li>Teaching students how to verify the correctness of their own code</li>
+                <li>Thoroughly explaining software design patterns</li>
+                <li>Pulling overtime office hours <span class='p-note'>otherwise most students wouldn't receive help</span></li>
+                <li>Manning an online help forum <span class='p-note'>email buckled under the volume of student inquiries</span></li>
+                <li>Giving advice I wish I had gotten early in my degrees</li>
+                <li>Helping students understand why and where they made mistakes</li>
+            </ul>
+            <br>
+            Helping transition the course toward modern paradigms
+            <ul class='p-list'>
+                <li>Moved away from Java Swing applications</li>
+                <li>Moved toward Java Spring REST APIs</li>
+            </ul>
         </p>
 
         <p class='p-text p-wide'>
-            Reading student code created a good opportunity to teach students.
-            From what students told me, I think they appreciated the feedback.
-            I also found that to be an enriching experience, especially when students told me they found it helpful to have someone earnestly read and comment on their work.
-            I have taken classes during my
-            <v-link local to='/projects/sosy'>Software Systems degree</v-link>
-            at SFU where I have written absolutely terrible code, and the TA marking my work never even commented on it... probably because they never read it.
-            The worst part about it was I didn't know how bad it was until looking back on it years later.
+            I think by the end I did a pretty great job.
+            I even received thank you cards and emails from many students saying they think I was the best TA they had ever met, not even just the best they had... the best they ever met.
         </p>
 
-        <p class='p-note p-thin'>
-            All that time I saved on marking allowed me to spend extra time reading students' code and giving feedback on code style and design choices.
+        <h2 id='why' class='p-wide p-sub-title'>
+            Why this project is important to me
+        </h2>
+
+        <p class='p-text p-wide'>
+            I don’t like to talk about myself that much.
+            I prefer to talk about the things I am interested in.
+            However, a portfolio is like a product page… my time, perspective, and knowledge are the products being sold, and as such those are what I need to talk about.
+            It was a fulfilling challenge to express who I am through the interactions, functionality, visuals, and content of this portfolio.
         </p>
+
+        <h2 id='how' class='p-wide p-sub-title'>
+            How I tackled it
+        </h2>
 
     </article>
 </template>
