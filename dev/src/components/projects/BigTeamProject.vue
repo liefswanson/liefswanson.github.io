94cb7f29d6f1ba3b63ca7b0886817efaf480f9d9
Lief Swanson <liefs@sfu.ca>
2018-11-18 05:23:21 -0800

    rewrite more projects

--- dev/src/components/projects/BigTeamProject.vue
+++ dev/src/components/projects/BigTeamProject.vue
@@ -1,100 +1,138 @@
 <template>
-<article class='p-article-grid'>
-    <h1 class='p-wide p-title'>
-        A Large Team Project for School
-    </h1>
+<project-template name='BigTeamProject'>
+    <template slot='intro'>
+        <p class='p-wide p-text'>
+            Anyone that has ever done a group project knows it is pretty common to get people that want to coast on your hard work.
+            That was not the case here. People worked pretty hard, but I think we bit off more than we could chew...
+        </p>
 
-    <p class='p-wide p-text'>
-        Anyone that has ever done a group project knows they can go pretty poorly.
-        It is pretty common to get people that want to coast on your hard work.
-        That was not the case here. People worked pretty hard, but I think we bit off more than we could chew...
-    </p>
+        <p class='p-thin p-note'>
+            Unfortunately, the code is not on my GitHub. I may share it there eventually, but it isn't all that impressive.
+            I think the experience is worth more than the resulting code.
+        </p>
 
-    <p class='p-thin p-note'>
-        Unfortunately, the code is not on my GitHub. I may share it there eventually, but it isn't all that impressive.
-        I think the experience is worth more than the resulting code.
-    </p>
+        <p class='p-wide p-text'>
+            This assignment in particular was a software development project where we had an actual client.
+            The client was from SFU's security desk, and they wanted an application built which would help them .
+            None of the requirements were known beforehand, and all the tasks were complex enough that we didn't really know how to effectively break them down so we could all work on them.
+            The class wasn't just about how to write code, but also how to work with other people so that work gets done effectively.
+        </p>
 
-    <p class='p-wide p-text'>
-        I think it is important to admit to the experiences you don't remember fondly.
-        Often those experiences have much to teach.
-        In this case, I learned a lot about how not to collaborate with a team of people.
-        It is really easy to be ineffective in communication with other people, causing everyone to waste time.
-    </p>
+        <p class='p-wide p-text'>
+            In the end it was a useful experience that I learned a great deal from.
+            I ended up using a lot of the skills I learned out of necessity during that project to
+            <router-link class='p-link' exact to='/projects/recursive-plug'>build this website</router-link>
+            you are looking at right now!
+        </p>
+    </template>
 
-    <p class='p-wide p-text'>
-        This assignment in particular was a software development project where we had an actual client.
-        The client had a thing they wanted built, and we took their requirements, and tried to make them real.
-        None of the requirements were known beforehand, and all the tasks were complex enough that we didn't really know how to effectively break them down so we could all work on them.
-        The class wasn't just about how to write code, but also how to work with other people so that work gets done effectively.
-    </p>
+    <template slot='what'>
+        <p class='p-wide p-text'>
+            I knew more than most about the tools we ended up using in this class, you wouldn't likely come across the tools we used in lower division computer science classes.
+            Most people were pretty far out of their comfort zone in our team, I think.
+            Which is why I ended up teaching the other people in our team how to use the tools we were working with.
+        </p>
 
-    <p class='p-thin p-note'>
-        This is easily the hardest task in software development.
-        Hence why we had a class in it.
-        It is an infamous problem that has sunk more projects than anything else in the world of software.
-        Check out a
-        <a class='p-link' href='https://www.goodreads.com/book/show/19288230-scrum'>book</a>
-        on this topic that I like very much.
-    </p>
+        <p class='p-thin p-note'>
+            Apparently I did a pretty good job of that, because by the end of the class the professor asked if I wanted to
+            <router-link class='p-link' exact to='/projects/teachers-assistant'>TA for another one of his classes</router-link>.
+        </p>
 
-    <p class='p-wide p-text'>
-        I think one of the things that was a mistake on our part was that we picked tools that were becoming popular in the industry, as opposed to the ones we thought would be the easiest to learn.
-    </p>
 
-    <p class='p-wide p-text'>
-        By the end we had to learn how to use:
-    </p>
+    </template>
 
-    <ul class='p-wide p-list'>
-        <li>JavaScript</li>
-        <li>SCSS <span class='p-note'>and later stylus</span></li>
-        <li>
-            Vue
-            <ul class='p-list'>
-                <li>Vue Router</li>
-                <li>VueX</li>
-                <li>Veutify</li>
-            </ul>
-        </li>
-        <li>SFU's Auth system</li>
-        <li>
-            Play Framework
-            <ul class='p-list'>
-                <li>EBean</li>
-                <li>Jackson</li>
-                <li>Play Router</li>
-            </ul>
-        </li>
-    </ul>
+    <template slot='why'>
+        <p class='p-wide p-text'>
+            I think it is important to admit to the experiences you don't remember fondly.
+            Often those experiences have much to teach.
+            In this case, I learned a lot about how not to collaborate with a team of people.
+            It is really easy to be ineffective in communication with other people, causing everyone to waste time.
+        </p>
 
-    <p class='p-thin p-note'>
-        Moreover, we learned all of those things in four months, while trying to figure out what our client actually wanted us to do... and each of us were taking other classes.
-    </p>
+        <p class='p-thin p-note'>
+            This is easily the hardest task in software development.
+            It is an infamous problem that has sunk more projects than anything else in the world of software.
+            Check out a
+            <a class='p-link' href='https://www.goodreads.com/book/show/19288230-scrum'>book</a>
+            on this topic that I like very much.
+        </p>
 
-    <p class='p-wide p-text'>
-        I knew more than most about these things, as none of them are things you would likely come across in lower division computer science classes.
-        Computer Science tends to be more focused on the theory, and at SFU the languages focused on most are C++ and Java.
-    </p>
+        <p class='p-wide p-text'>
+            Ultimately, I think we did a poor job of finding a way for everyone to work on something.
+            At first we tried specializing, and that didn't work all that well, due to our erratic student schedules.
+            Often we ended up getting bottlenecked by other peoples' schedules.
+            There was one thing I found worked though, and it has yet to let me down...
+            <a class='p-link' href='https://en.wikipedia.org/wiki/Pair_programming'>pair programming</a>.
+        </p>
 
-    <p class='p-wide p-text'>
-        Ultimately, I think we did a poor job of finding a way for everyone to work on something.
-        At first we tried specializing, and that didn't work all that well, due to our erratic student schedules.
-        Often we ended up getting bottlenecked by other peoples' schedules.
-        There was one thing I found worked though, and it has yet to let me down...
-        <a class='p-link' href='https://en.wikipedia.org/wiki/Pair_programming'>pair programming</a>.
-    </p>
+        <p class='p-thin p-note'>
+            I am a huge fan of pair programming, as it allows two people to work on literally the same thing.
+            I find it often reduces stress and improves quality of work while increasing speed.
+        </p>
 
-    <p class='p-thin p-note'>
-        I am a huge fan of pair programming, as it allows two people to work on literally the same thing.
-        I find it often reduces stress and improves quality of work while increasing speed.
-    </p>
+        <p class='p-wide p-text'>
+            I think another mistake on our part was we picked tools that were becoming popular in the industry, as opposed to the ones we thought would be the easiest to learn.
+        </p>
 
-    <p class='p-wide p-text'>
-        In the end it was a good experience that I learned a great deal from.
-        I ended up using a lot of the skills I learned out of necessity during that project here on this website you are looking at right now!
-    </p>
-</article>
+        <p class='p-wide p-text'>
+            By the end we had to learn how to use:
+        </p>
+
+        <ul class='p-wide p-list'>
+            <li>JavaScript</li>
+            <li>SCSS <span class='p-note'>and later stylus</span></li>
+            <li>
+                Vue
+                <ul class='p-list'>
+                    <li>Vue Router</li>
+                    <li>VueX</li>
+                    <li>Veutify</li>
+                </ul>
+            </li>
+            <li>SFU's Auth system</li>
+            <li>
+                Play Framework
+                <ul class='p-list'>
+                    <li>EBean</li>
+                    <li>Jackson</li>
+                    <li>Play Router</li>
+                </ul>
+            </li>
+        </ul>
+
+        <p class='p-wide p-text'>
+            Moreover, we learned all of those things in four months, while trying to figure out what our client actually wanted us to do... and each of us were taking other classes.
+        </p>
+    </template>
+
+    <template slot='how'>
+        <p class='p-wide p-text'>
+            I think the only thing that kept us on track at all was that we were forced to use a light form of
+            <a class='p-link' href="https://www.scrum.org/resources/what-is-scrum">scrum</a>
+            during our project.
+            We were forced to have weekly meetings, and we were also forced to keep track of things on our own gitlab issue tracker.
+        </p>
+
+        <p class='p-wide p-text'>
+            We unfortunately did not do a very good job of organizing, and I think this came down partly to us relying too much on applications like
+            <a class='p-link' href="https://slack.com">slack</a>.
+            There is a lot of urgency that is lost when communicating asynchronously, and the forum like discussions were a far worse way of organizing than just creating issues in our team issue tracker.
+            When our whole team met in person, more work definitely got done, as well as when I would meet online in video calls with my teammate.
+            We sort of grouped ourselves in the buddy system, and not all team members kept to it, but my buddy and I did, and that kept us reasonably focused and organized... at least between the two of us.
+        </p>
+
+        <p class='p-thin p-note'>
+            While apps like slack preport to improve team efficiency, I think that is not really true...
+            In theory they sound great, but communication in a style similar to texting is pretty inefficient.
+            Worse yet, notifications are a huge productivity killer; every time you receive a notification it pulls you out of what you are doing, and it takes time to get back to up and running.
+        </p>
+
+        <p class='p-wide p-text'>
+            So, if I had to summarize... how did I tackle this? I would say ineffectively.
+            But... I can say that I know quite clearly where our team fell short.
+        </p>
+    </template>
+</project-template>
 </template>
 
 <script lang="ts">
--- dev/src/components/projects/SoSy.vue
+++ dev/src/components/projects/SoSy.vue
@@ -1,91 +1,139 @@
 <template>
-<article class='p-article-grid'>
-    <h1 class='p-wide p-title'>
-        Software Systems (SoSy) Undergrad at SFU
-    </h1>
+<project-template name='SoSy'>
+    <template slot='intro'>
+        <p class='p-wide p-text'>
+            One of my bachelor's of science from
+            <a class='p-link' title='Simon Fraser University' href='https://www.sfu.ca/'>SFU</a>
+            was in Computer Science, with a focus on Software Systems.
+            In it I learned about the theory and practice of designing software and manipulating computers.
+        </p>
 
-    <p class='p-wide p-highlight'>
-        Check out the
-        <a class='p-link' href='http://www.sfu.ca/computing/prospective-students/undergraduate-students/programs/degree-programs/softwaresystems/curriculum.html'>Curriculum<i class='fas fa-graduation-cap p-icon'/>
-        </a>
-    </p>
+        <p class='p-wide p-text'>
+            If you want, you can check out the
+            <a class='p-link' href='http://www.sfu.ca/computing/prospective-students/undergraduate-students/programs/degree-programs/softwaresystems/curriculum.html'>Curriculum<i class='fas fa-graduation-cap p-icon'/>
+            </a>
+        </p>
+    </template>
 
-    <p class='p-wide p-text'>
-        Software Systems is like the applied version of computing science.
-        At SFU both are offered, but I took Software Systems because I wanted to have more experience actually making software!
-        I am actually pretty good at computing science theory, and have taken some interesting courses in it.
-        However, I find I am more interested in making things that people want to use than anything else.
-    </p>
+    <template slot='what'>
+        <p class='p-wide p-text'>
+            Software Systems is like the applied version of computing science.
+            At SFU both are offered, but I took Software Systems because I wanted to have more experience actually making software!
+            I am actually pretty good at computing science theory, and have taken some interesting courses in it.
+            However, I find I am more interested in making things that people want to use than anything else.
+        </p>
 
-    <p class='p-wide p-text'>
-        I chose Software Systems for that exact reason.
-        When I first came to SFU, I actually came for my other major.
-        I didn't even know that programming existed.
-        I had no concept of what programming even was.
-        However, in first year I was required to take a course in Python as part of my other major
-        <router-link exact to='/projects/siat'>SIAT</router-link>.
-    </p>
+        <p class='p-thin p-note'>
+            I chose Software Systems for that exact reason.
+            When I first came to SFU, I actually came for my other major.
+            I didn't even know that programming existed;
+            I had no concept of what programming even was.
+            However, in first year I was required to take a computing science course in Python as part of my other major
+            <router-link exact to='/projects/siat'>SIAT</router-link>.
+        </p>
 
-    <p class='p-wide p-text'>
-        It quickly dawned on me that if I wanted to go to school for design, I should probably also learn how to implement the things I wanted to design.
-        So, I went into a double major.
-        I haven't regretted it at all.
-    </p>
+        <p class='p-wide p-text'>
+            Here is an <b>incomplete</b> list of the topics I have studied in my SoSy degree:
+        </p>
 
-    <p class='p-wide p-text'>
-        Here is an <b>incomplete</b> list of the topics I have studied in my SoSy degree:
-    </p>
+        <ul class='p-wide p-list'>
+            <li>Computational complexity <span class='p-note'>and algorithm analysis</span></li>
+            <li>Computational geometry</li>
+            <li>Compilers
+                <span class='p-note'>which you can see
+                    <router-link class='p-link' exact to='/static/projects/writing-a-compiler'>here</router-link>
+                </span>
+            </li>
+            <li>Graphics
+                <ul class='p-list'>
+                    <li>
+                        Raster graphics and
+                        <router-link class='p-link' exact to='/projects/tetris'>writing a game in openGL</router-link>
+                    </li>
+                    <li>
+                        <router-link class='p-link' exact to='/projects/ray-tracer'>Implementing a ray tracer</router-link>
+                    </li>
+                </ul>
+            </li>
+            <li>Design patterns</li>
+            <li>Data Structures</li>
+            <li>Databases</li>
+            <li>Object oriented programming</li>
+            <li>Functional programming</li>
+            <li>Networking</li>
+            <li>
+                Algorithms for:
+                <ul class='p-list'>
+                    <li>Compression</li>
+                    <li>Media manipulation</li>
+                    <li>Dynamic Programming</li>
+                    <li>Sorting</li>
+                    <li>Pathfinding</li>
+                    <li>NP Hard problems</li>
+                    <li>Geometry problems</li>
+                </ul>
+            </li>
+            <li>Operating systems</li>
+            <li>Quality assurance</li>
+            <li>Logic circuits</li>
+            <li>Technical writing</li>
+            <li>Requirements gathering</li>
+        </ul>
 
-    <ul class='p-wide p-list'>
-        <li>Computational complexity <span class='p-note'>and algorithm analysis</span></li>
-        <li>Computational geometry</li>
-        <li>Compilers
-            <span class='p-note'>which you can see
-                <router-link exact to='/static/projects/writing-a-compiler'>here</router-link>
-            </span>
-        </li>
-        <li>Graphics</li>
-        <li>Design patterns</li>
-        <li>Data Structures</li>
-        <li>Databases</li>
-        <li>Object oriented programming</li>
-        <li>Functional programming</li>
-        <li>Networking</li>
-        <li>
-            Algorithms for:
-            <ul class='p-list'>
-                <li>Compression</li>
-                <li>Media manipulation</li>
-                <li>Dynamic Programming</li>
-                <li>Sorting</li>
-                <li>Pathfinding</li>
-                <li>NP Hard problems</li>
-                <li>Geometry problems</li>
-            </ul>
-        </li>
-        <li>Operating systems</li>
-        <li>Quality assurance</li>
-        <li>Hardware design</li>
-        <li>Technical writing</li>
-        <li>Requirements gathering</li>
-    </ul>
+        <p class='p-thin p-note'>
+            More importantly though, I think the best part of being at SFU has been the people that I have met.
+            Many of them introduced me to things I didn't even know existed.
+            The classes are fine, but definitely the people have taught me more.
+        </p>
+    </template>
 
-    <p class='p-thin p-note'>
-        More importantly though, I think the best part of being at SFU has been the people that I have met.
-        Many of them introduced me to things I didn't even know existed.
-        The classes are fine, but definitely the people have taught me more.
-    </p>
+    <template slot='why'>
+        <p class='p-text p-wide'>
+            Computer science has been a window into many other fields for me.
+            Many of the best things in computer science were taken unapologetically from other disciplines.
+            That sort of willingness to learn from other fields fits the way I view the world quite well.
+        </p>
+        <p class='p-text p-wide'>
+            I think that I found the job that I most enjoy in software engineering... I honestly can't stop nerding out about it.
+            I am also found of many other fields, but the nice thing about software engineering is it is a means to many ends.
+            The ability to manipulate a computer to do what you want is an extraordinarily valuable skill, and I am glad to have spent the time discovering it.
+            As I said, before my first semester of university, I had no concept of what went into making software so to me this degree was important... without it I may have never discovered software engineering.
+        </p>
+        <p class='p-note p-thin'>
+            That is also what I like about design, design is also a means to many ends.
+        </p>
+    </template>
 
-</article>
+    <template slot='how'>
+        <p class='p-text p-wide'>
+            Lots of perseverance.
+            I think getting the hang of computer science was one of the hardest things I have ever done.
+            I have always liked solving problems, but computer science and software development are on a different level to what I was accustom to before that point.
+            Everything I had ever tackled before that point was easy to visualize, and I am primarily a visual thinker.
+            Computer logic is not easy to visualize, at least I don't think so.
+            It took a lot of work to get to the point that I felt thinking about my code was second nature.
+        </p>
+        <p class='p-text p-wide'>
+            There are so many things that are counter-intuitive about computer science... even the math that sits at its backbone is counter-intuitive.
+            I always found calculus and geometry to be easy to pick up, because I can <strong>see</strong> them.
+            Discrete math, computer logic, and statistics are a different beast, in that they are inherently hard to visualize.
+            I had to learn how to use my tools to aid my visual thinking processes, without that I would never have made it.
+        </p>
+    </template>
+</project-template>
 </template>
 
 
 <script lang="ts">
 import Vue from 'vue';
+import ProjectTemplate from "@/components/util/ProjectTemplate.vue";
+import Carousel from "@/components/util/Carousel.vue";
 
 export default Vue.extend({
-    name: 'SoSy',
-    components: {
-    }
+  name: "SoSy",
+  components: {
+      'project-template': ProjectTemplate,
+      'v-carousel': Carousel
+  }
 });
 </script>
