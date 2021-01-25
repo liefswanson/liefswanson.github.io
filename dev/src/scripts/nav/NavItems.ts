9ba651e40bf3b24cd387f3be1b335d1ca64f6fc0
Lief Swanson <cixelsyd.guy@gmail.com>
2021-01-25 14:09:59 -0800

    update resume

--- dev/src/scripts/nav/NavItems.ts
+++ dev/src/scripts/nav/NavItems.ts
@@ -1,7 +1,6 @@
 import Section from './Section'
 
 import Projects from '@/components/main/Projects.vue';
-//import Ongoing  from '@/components/main/Ongoing.vue';
 import Resume   from '@/components/main/Resume.vue';
 import Contact  from '@/components/main/Contact.vue';
 import About    from '@/components/main/About.vue';
@@ -15,12 +14,6 @@ const Sections = [
         color: Swatch.projects,
         component: Projects,
     },
-    // {
-    //     path: "/ongoing",
-    //     name: "Ongoing",
-    //     color: Swatch.ongoing,
-    //     component: Ongoing
-    // },
     {
         path: "/contact",
         name: "Contact",
@@ -43,8 +36,6 @@ const Sections = [
 
 // NOTE: the method signature is true evil, this is better to infer it
 const SectionMap = {
-    // ongoing: Sections.filter(
-    //     (elem) => elem.component == Ongoing )[0],
     projects: Sections.filter(
         (elem) => elem.component == Projects)[0],
     resume:   Sections.filter(
