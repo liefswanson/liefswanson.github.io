83227bf6d5e7473d3952d21894372072c9373119
Lief Swanson <cixelsyd.guy@gmail.com>
2021-12-16 21:34:26 -0800

    remove old emails from git history.

---
+++ dev/src/scripts/main/Contacts.ts
@@ -0,0 +1,34 @@
+import Contact from '@/scripts/main/Contact';
+
+const Contacts: Contact[] = [
+    {
+        name: 'cixelsyd.guy@gmail.com',
+        fa: 'fa',
+        icon: 'fa-envelope-square',
+        msg: 'email',
+        link: 'cixelsyd.guy@gmail.com'
+    },
+    {
+        name: 'github',
+        fa: 'fab',
+        icon: 'fa-github',
+        msg: 'fork me on',
+        link: 'https://github.com/liefswanson'
+    },
+    {
+        name: 'flickr',
+        fa: 'fab',
+        icon: 'fa-flickr',
+        msg: 'peek at my',
+        link: 'https://www.flickr.com/photos/124866205@N05'
+    },
+    {
+        name: 'linkedin',
+        fa: 'fab',
+        icon: 'fa-linkedin',
+        msg: 'find me on',
+        link: 'https://www.linkedin.com/pub/lief-swanson/106/531/a7a'
+    }
+];
+
+export default Contacts;
